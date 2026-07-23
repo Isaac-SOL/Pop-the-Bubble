525a91ff1d05859016cd70d6f468432acd81ab29
Cryptal <99368189+cryptale@users.noreply.github.com>
2026-07-23 19:44:20 +0200

    Spawn
    Add random spawn on Bubble

--- scripts/objects/bubbles/bubble_spawner.gd
+++ scripts/objects/bubbles/bubble_spawner.gd
@@ -1,9 +1,11 @@
 extends Bubble
 class_name BubbleSpawner
+@export var taux_de_spawn: int = 72 #pourcentage
 
 func _ready() -> void:
 	super()
 	shader_material.set_shader_parameter("wobble_strenght", 0.05)
 
 func _on_timer_timeout() -> void:
-	spawn.emit(1)
+	if randi() % 101 > taux_de_spawn:
+		spawn.emit(1, position, 0)
