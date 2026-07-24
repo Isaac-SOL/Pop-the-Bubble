e69a973dbe0146c9448909a0b3ce0b6e7afa1fd3
NicolasR-Pro <lamarouj@laposte.net>
2026-07-24 12:56:11 -0300

    Fixed a bug on old spawn bubble instantiating

--- scripts/objects/bubbles/bubble.gd
+++ scripts/objects/bubbles/bubble.gd
@@ -155,10 +155,7 @@ func bubble_popped()-> void:
 		if is_factory:
 			Global.factory_bubble_count -=1
 			
-		AudioManager.playAudio_stream_sfx(&"bubble_pop")
-		if bubble_level > 3:
-			Global.main_node.spawn_bubble(self.global_position, bubble_level-2, bubble_level, Global.main_node.BUBBLE_SPAWNER)
-			
+		AudioManager.playAudio_stream_sfx(&"bubble_pop")			
 		popped.emit(false)
 	
 func bubble_deleted()-> void:
