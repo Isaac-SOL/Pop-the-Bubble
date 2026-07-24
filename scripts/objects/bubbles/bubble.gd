f895353a483db95c54a65f1c43f5feffd2f8b3c7
NicolasR-Pro <lamarouj@laposte.net>
2026-07-24 20:10:56 -0300

    Reforged the powers to be only bubble, refactor some bubble code, added sfx for nugget collect, added the internet bubble

--- scripts/objects/bubbles/bubble.gd
+++ scripts/objects/bubbles/bubble.gd
@@ -6,15 +6,17 @@ signal popped(is_deleted : bool)
 @warning_ignore("unused_signal")
 signal spawn(amount: int, pos: Vector2, level: int)
 
-@onready var sprite_2d: Sprite2D = $bubble_corps/Sprite2D
-@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $bubble_corps/VisibleOnScreenNotifier2D
 @onready var audio_stream_player_2d_bubble_up: AudioStreamPlayer2D = %AudioBubbleUp
+@onready var sprite_2d: Sprite2D = %Sprite2D
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = %VisibleOnScreenNotifier2D
 
 
 @export var bubble_level: int = 0
 @export var bubble_color: Color = Color.WHITE
 @export var speed_mult_at_start = 6.0
 
+const DEFAULT_SPAWN_RATE : int = 25
+const DEFAULT_FACTORY_MAX_RANGE_TIMER : float = 5.0
 
 var shader_material : ShaderMaterial
 var speed : float
@@ -22,13 +24,22 @@ var velocity: Vector2
 var bubble_types: Array
 var speed_start_mult: float = 1.0
 var stonk_count: int = 0
+
 var is_stonk: bool = false
 var is_speculative: bool = false
 var is_dividend: bool = false
-var timer_dividend: Timer
 var is_factory: bool = false
+var is_internet: bool = false
+var is_gpt: bool = false
+var is_storm: bool = false
+var is_metaverse: bool = false
+
 var timer_factory: Timer
-var factory_spawn_rate: int = 75
+var timer_metaverse: Timer
+var timer_dividend: Timer
+
+var factory_max_range_timer : float = DEFAULT_FACTORY_MAX_RANGE_TIMER
+var spawn_rate: int = DEFAULT_SPAWN_RATE
 var nugget_value: int
 var dead: bool = false
 
@@ -39,47 +50,51 @@ func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(bubble_deleted)
 	scale = Vector2(1.0+bubble_level/1.5, 1.0+bubble_level/1.5)
 	nugget_value = (bubble_level+1)*3
-	if bubble_color == Color.WHITE:
-		match bubble_level:
-			0:
-				bubble_color = Color.SKY_BLUE
-				set_collision_layer_value(3, false)
-				set_collision_mask_value(3, false)
-			1:
-				bubble_color = Color.DARK_BLUE
-			2:
-				bubble_color = Color.REBECCA_PURPLE
-			3:
-				bubble_color = Color.RED
-			4:
-				bubble_color = Color.DARK_RED
-			5:
-				scale = Vector2.ZERO
-				var scale_tween := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
-				scale_tween.tween_property(self, "scale", Vector2.ONE * 7, 2.0)
+	match bubble_level:
+		0:
+			set_color(Color.SKY_BLUE)
+			set_collision_layer_value(3, false)
+			set_collision_mask_value(3, false)
+		1:
+			set_color(Color.CADET_BLUE)
+		2:
+			set_color(Color.DARK_BLUE)
+		3:
+			set_color(Color.PURPLE)
+		4:
+			set_color(Color.REBECCA_PURPLE)
+		5:
+			set_color(Color.WHITE)
+			scale = Vector2.ZERO
+			var scale_tween := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
+			scale_tween.tween_property(self, "scale", Vector2.ONE * 7, 2.0)
 	
-	shader_material.set_shader_parameter("bubble_color", bubble_color)
-	shader_material.set_shader_parameter("rim_color", bubble_color+Color(0.1,0.1,0.1,0.0))
 	velocity = Vector2(randf_range(-1.0,1.0),randf_range(-1.0,1.0))
-	speed = randf_range(50.0,200.0) / scale.x
+	speed = randf_range(50.0,200.0) / (1.0+bubble_level/1.5)
 	
 	if bubble_types.size() > 0:
 		for type in bubble_types:
 			match type:
-				PowerManager.BUBBLE_DIVIDEND:
+				BubbleManager.BUBBLE_DIVIDEND:
 					set_bubble_dividend()
-				PowerManager.BUBBLE_FACTORY:
+				BubbleManager.BUBBLE_FACTORY:
 					set_bubble_factory()
-				PowerManager.BUBBLE_SPECULATIVE:
+				BubbleManager.BUBBLE_SPECULATIVE:
 					set_bubble_speculative()
-				PowerManager.BUBBLE_STONK:
+				BubbleManager.BUBBLE_STONK:
 					set_bubble_stonk()
+				BubbleManager.BUBBLE_GPT:
+					set_bubble_gpt()
+				BubbleManager.BUBBLE_METAVERSE:
+					set_bubble_metaverse()
+				BubbleManager.BUBBLE_INTERNET:
+					set_bubble_internet()
 	
 	
 func _physics_process(delta: float) -> void:
 	if dead:
 		return
-	position += velocity * speed * speed_start_mult * PowerManager.bubble_speed_mult * delta
+	position += velocity * speed * speed_start_mult * BubbleManager.bubble_speed_mult * delta
 	if is_speculative:
 		scale += Vector2(delta,delta)
 	
@@ -101,44 +116,102 @@ func set_bubble_stonk(stonk_value: int = 3)-> void:
 		is_stonk = true
 		Global.stonk_bubble_count += 1
 		stonk_count = stonk_value
-		shader_material.set_shader_parameter("rim_color", Color.RED)
+		set_color(Color.DARK_GREEN)
 		
 func set_bubble_speculative()-> void:
 	is_speculative = true
 	Global.speculative_bubble_count += 1
-	shader_material.set_shader_parameter("rim_color", Color.GREEN)
+	set_color(Color.GREEN)
 	set_collision_layer_value(3, false)
 	set_collision_mask_value(3, false)
 		
 func set_bubble_dividend()-> void:
 	is_dividend = true
 	Global.dividend_bubble_count += 1
-	shader_material.set_shader_parameter("rim_color", Color.YELLOW)
-	shader_material.set_shader_parameter("wobble_strenght", 0.2)
+	spawn_rate = 0
+	set_color(Color.ORANGE_RED)
+	shader_material.set_shader_parameter("wobble_strength", 0.05)
 	timer_dividend = Timer.new()
 	timer_dividend.one_shot = true
 	add_child(timer_dividend)
-	timer_dividend.timeout.connect(create_dividend_bubble)
-	timer_dividend.start(randf_range(3.0,8.0))
+	timer_dividend.timeout.connect(create_bubble.bind(timer_dividend, Vector2(7.0,7.0), true, 0, 1, [BubbleManager.BUBBLE_DIVIDEND]))
+	timer_dividend.start(7.0)
 	
 func set_bubble_factory()-> void:
 	is_factory = true
 	Global.factory_bubble_count += 1
-	shader_material.set_shader_parameter("rim_color", Color.HOT_PINK)
-	shader_material.set_shader_parameter("wobble_strenght", 0.1)
+	spawn_rate = 50
+	set_color(Color.HOT_PINK)
+	shader_material.set_shader_parameter("wobble_strength", 0.05)
 	timer_factory = Timer.new()
 	timer_factory.one_shot = true
 	add_child(timer_factory)
-	timer_factory.timeout.connect(create_factory_bubble)
-	timer_factory.start(randf_range(3.0,8.0))
-
-func create_dividend_bubble()-> void:
-		Global.main_node.spawn_bubble(Vector2(position.x+randf_range(-1.0,1.0), position.y+randf_range(-1.0,1.0)), bubble_level, 1, [PowerManager.BUBBLE_DIVIDEND])
-		timer_dividend.start(randf_range(3.0,8.0))
+	timer_factory.timeout.connect(create_bubble.bind(timer_factory, Vector2(1.0,factory_max_range_timer), true, 1, 2))
+	timer_factory.start(1.0)
+	
+func set_bubble_internet(activate: bool = true)-> void:
+	if activate:
+		is_internet = true
+		Global.internet_bubble_count += 1
+		set_color(Color.GRAY)
+		shader_material.set_shader_parameter("wobble_strength", 0.05)
+		for bubble : Bubble in Global.all_bubbles:
+			if bubble.is_factory:
+				bubble.spawn_rate = 0
+				bubble.factory_max_range_timer = 1.5
+	else:
+		for bubble : Bubble in Global.all_bubbles:
+			if bubble.is_factory:
+				bubble.pawn_rate = bubble.DEFAULT_SPAWN_RATE
+				bubble.factory_max_range_timer = bubble.DEFAULT_FACTORY_MAX_RANGE_TIMER
 		
-func create_factory_bubble()-> void:
-		Global.main_node.spawn_bubble(Vector2(position.x+randf_range(-1.0,1.0), position.y+randf_range(-1.0,1.0)), 1, 2)
-		timer_factory.start(randf_range(3.0,8.0))
+	
+func set_bubble_gpt(activate: bool = true)-> void:
+	if activate:
+		is_gpt = true
+		Global.gpt_bubble_count += 1
+		set_color(Color.RED)
+		shader_material.set_shader_parameter("wobble_strength", 0.05)
+		Global.main_node.player_hand.area.set_collision_layer_value(5, true)
+	else:
+		Global.main_node.player_hand.area.set_collision_layer_value(5, false)
+	
+func set_bubble_metaverse()-> void:
+	is_metaverse = true
+	Global.metaverse_bubble_count += 1
+	spawn_rate = 75
+	set_color(Color.ORANGE)
+	shader_material.set_shader_parameter("wobble_strength", 0.05)
+	timer_metaverse = Timer.new()
+	timer_metaverse.one_shot = true
+	add_child(timer_metaverse)
+	timer_metaverse.timeout.connect(create_bubble.bind(timer_metaverse, Vector2(7.0,9.0), false, 4, 1))
+	timer_metaverse.start(3.0)
+		
+	
+func set_bubble_storm(activate: bool = true)-> void:
+	if activate:
+		is_storm = true
+		Global.storm_bubble_count += 1
+		set_color(Color.YELLOW)
+		shader_material.set_shader_parameter("wobble_strength", 0.05)
+		BubbleManager.bubble_speed_mult = 3.0
+	else:
+		BubbleManager.bubble_speed_mult = 1.0
+	
+
+
+func create_bubble(timer: Timer, timer_range : Vector2, is_self_pos : bool, level: int, qty: int = 1, types: Array = [])-> void:
+	#25% luck to spawn bubble with spawn_rate = 75
+	if randi() % 101 > spawn_rate:
+		var pos : Vector2
+		if is_self_pos:
+			pos = Vector2(position.x+randf_range(-1.0,1.0), position.y+randf_range(-1.0,1.0))
+		else:
+			pos = Util.rand_in_rectangle(Global.main_node.spawn_rect)
+		Global.main_node.spawn_bubble(pos, level, qty, types)
+	timer.start(randf_range(timer_range.x, timer_range.y))
+
 
 	
 
@@ -151,21 +224,16 @@ func bubble_popped()-> void:
 		audio_stream_player_2d_bubble_up.play()
 		audio_stream_player_2d_bubble_up.pitch_scale += 0.2
 	else:
-		if is_stonk:
-			Global.stonk_bubble_count -= 1
-		if is_speculative:
-			Global.speculative_bubble_count -= 1
-		if is_dividend:
-			Global.dividend_bubble_count -= 1
-		if is_factory:
-			Global.factory_bubble_count -=1
-			
+		remove_bubble()
 		AudioManager.playAudio_stream_sfx(&"bubble_pop")			
 		popped.emit(false)
 	
 func bubble_deleted()-> void:
 	if dead:
 		return
+	remove_bubble()
+	
+func remove_bubble()-> void:
 	if is_stonk:
 		Global.stonk_bubble_count -= 1
 	if is_speculative:
@@ -174,7 +242,26 @@ func bubble_deleted()-> void:
 		Global.dividend_bubble_count -= 1
 	if is_factory:
 		Global.factory_bubble_count -=1
-	popped.emit(true)
+	if is_gpt:
+		Global.gpt_bubble_count -=1
+		if Global.gpt_bubble_count <= 0:
+			set_bubble_gpt(false)
+	if is_storm:
+		Global.storm_bubble_count -=1
+		if Global.storm_bubble_count <= 0:
+			set_bubble_storm(false)
+	if is_metaverse:
+		Global.metaverse_bubble_count -=1
+	if is_internet:
+		Global.internet_bubble_count -=1
+		if Global.internet_bubble_count <= 0:
+			set_bubble_internet(false)
+	
+func set_color(color: Color):
+	bubble_color = color
+	shader_material.set_shader_parameter("bubble_color", bubble_color)
+	shader_material.set_shader_parameter("rim_color", bubble_color+Color(0.1,0.1,0.1,0.0))
+	
 
 func on_spawn():
 	speed_start_mult = speed_mult_at_start
@@ -185,7 +272,7 @@ func pop_animation():
 	dead = true
 	set_deferred("collision_layer", 0)
 	set_deferred("collision_mask", 0)
-	%bubble_corps.hide()
+	sprite_2d.hide()
 	%Splash.show()
 	%Splash.scale = Vector2.ONE * 0.365
 	var splash_tween := create_tween().set_trans(Tween.TRANS_QUAD)
--- scripts/ui/ui_player.gd
+++ scripts/ui/ui_player.gd
@@ -6,6 +6,10 @@ extends Control
 @onready var label_bubbles: Label = %LabelBubbles
 @onready var label_bubbles_speculative: Label = %LabelBubblesSpeculative
 @onready var label_bubbles_dividend: Label = %LabelBubblesDividend
+@onready var label_bubbles_iternet: Label = %LabelBubblesIternet
+@onready var label_bubbles_storm: Label = %LabelBubblesStorm
+@onready var label_bubbles_gpt: Label = %LabelBubblesGPT
+@onready var label_bubbles_metaverse: Label = %LabelBubblesMetaverse
 
 func _physics_process(_delta: float) -> void:
 	SettingsManager.apply_mouse_scale()
@@ -30,12 +34,32 @@ func _physics_process(_delta: float) -> void:
 		
 	if Global.dividend_bubble_count > 0:
 		label_bubbles_dividend.show()
-		label_bubbles_dividend.text = "Speculative Bubbles: "+str(Global.dividend_bubble_count)
+		label_bubbles_dividend.text = "Dividend Bubbles: "+str(Global.dividend_bubble_count)
 	else:
 		label_bubbles_dividend.hide()
 		
-	if Global.all_bubbles.size() > 0:
-		label_bubbles.show()
-		label_bubbles.text = "Total Bubbles: "+str(Global.all_bubbles.size())
+	if Global.internet_bubble_count > 0:
+		label_bubbles_iternet.show()
+		label_bubbles_iternet.text = "Internet Bubbles: "+str(Global.internet_bubble_count)
 	else:
-		label_bubbles.hide()
+		label_bubbles_iternet.hide()
+		
+	if Global.storm_bubble_count > 0:
+		label_bubbles_storm.show()
+		label_bubbles_storm.text = "Storm Bubbles: "+str(Global.storm_bubble_count)
+	else:
+		label_bubbles_storm.hide()
+		
+	if Global.gpt_bubble_count > 0:
+		label_bubbles_gpt.show()
+		label_bubbles_gpt.text = "GPT Bubbles: "+str(Global.gpt_bubble_count)
+	else:
+		label_bubbles_gpt.hide()
+		
+	if Global.metaverse_bubble_count > 0:
+		label_bubbles_metaverse.show()
+		label_bubbles_metaverse.text = "Metaverse Bubbles: "+str(Global.metaverse_bubble_count)
+	else:
+		label_bubbles_metaverse.hide()
+		
+	label_bubbles.text = "Total Bubbles: "+str(Global.all_bubbles.size())
