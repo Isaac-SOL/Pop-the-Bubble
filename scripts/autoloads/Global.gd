38dbdda56b0dab24a5a97b0e37bd9a5890b394b8
NicolasR-Pro <lamarouj@laposte.net>
2026-07-25 17:16:10 -0300

    Updated all the special bubble, refactored code linkend to the bubbles, added bubble counter

--- scripts/autoloads/Global.gd
+++ scripts/autoloads/Global.gd
@@ -5,15 +5,18 @@ var bubble_per_seconds : float = 0
 
 var all_bubbles: Array[Bubble] = []
 var bubble_count : int = 0
+var nugget_collected : int = 0
+#var storm_bubble_count : int = 0
+#var metaverse_bubble_count : int = 0
 var stonk_bubble_count : int = 0
 var dividend_bubble_count : int = 0
 var speculative_bubble_count : int = 0
 var factory_bubble_count : int = 0
 var internet_bubble_count : int = 0
 var gpt_bubble_count : int = 0
-var metaverse_bubble_count : int = 0
-var nugget_collected : int = 0
-var storm_bubble_count : int = 0
+var spike_bubble_count : int = 0
+var crash_bubble_count : int = 0
+var shielding_bubble_count : int = 0
 
 var main_node: Node
 func set_main_reference(node: Node)-> void:
